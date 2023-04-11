fe263ba8207817f9e21285d83b29229756c1e0fb
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-04-11 19:01:29 +0300

    Initial Commit

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,46 @@
+﻿public static class Result
+{
+    public static int MajorityElement(int[] nums)
+    {
+        Array.Sort(nums);
+        int counter = 1;
+        int result = 0;
+        if (nums.Length == 1)
+        {
+            result = nums[0];
+        }
+        else
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        counter++;
+                        if (counter > nums.Length / 2)
+                        {
+                            result = nums[i];
+                            return result;
+                        }
+                    }
+                    else { counter = 1; }
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int[] nums = { 3, 2, 3 };
+        Console.WriteLine(Result.MajorityElement(nums));
+    }
+
+}
