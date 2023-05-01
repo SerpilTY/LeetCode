23cb9a2c7466405493d0a4fac7b2ac79ab518c64
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-05-01 23:11:10 +0300

    Initial Commit

---
+++ TheKthFactorOfN/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int KthFactor(int n, int k) {
+        List<int> FactorList=new List<int>();
+        for (var i = 1; i <= n; i++)
+        {
+            if(n%i==0){
+                FactorList.Add(i);
+            }
+        }
+
+        if(k<=FactorList.Count){
+            return FactorList[k-1];
+        }
+        else return -1;
+
+}
+
+
+        public static void Main(string[] args)
+        {
+            int n = 6;
+            int k=2;
+            Solution soly=new Solution();
+            System.Console.WriteLine(soly.KthFactor(n,k));
+
+        }
+
+    }
+
