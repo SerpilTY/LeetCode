ada1328dd9d1d1f4b23ab697e643354d84120f99
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-05-30 23:14:37 +0300

    Initial Commit

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs[0] == "") return "";
+        string prefix = "";
+        Array.Sort(strs, (x, y) => x.Length.CompareTo(y.Length));
+        int count = 0;
+        for (var i = 0; i < strs[0].Length; i++)
+        {
+            char mychar = strs[0][i];
+            foreach (var item in strs)
+            {
+                if (mychar == item[i])
+                {
+                    count++;
+                    if (count == strs.Length)
+                    {
+                        count = 0;
+                        prefix += mychar;
+                    }
+                }
+                else { count = -1; break; }
+            }
+            if (count == -1)
+                break;
+        }
+
+        return prefix;
+    }
+}
+class Result
+{
+    public static void Main(string[] args)
+    {
+        Solution soly = new Solution();
+        string[] stry = { "flower", "flawer", "flvwer", "flower" };
+        Console.WriteLine(soly.LongestCommonPrefix(stry));
+
+    }
+
+}
